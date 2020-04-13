32a9d9f02de83b71575ec28aa11ecb1ddb6c3146
Thomas Smith <thsm@umich.edu>
2020-04-13 16:57:41 -0400

    Initial Commit

---
+++ SortMyGroceries/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SortMyGroceries
+//
+//  Created by Thomas Smith on 4/13/20.
+//  Copyright © 2020 Thomas Smith. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
